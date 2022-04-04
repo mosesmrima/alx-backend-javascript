09dfea8fd090df038d8aad35aafbe2e5a8b5a2a0
mosesmrima <mrimamss@gmail.com>
2022-04-04 23:09:05 +0300

    append to array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (let idx of array) {
+    const value = idx;
+    idx = appendString + value;
+    newArr.push(idx);
+  }
+
+  return newArr;
+}
