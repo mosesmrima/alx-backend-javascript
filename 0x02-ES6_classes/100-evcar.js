0a9cddc837dec231397147e70369706c32295dac
mosesmrima <mrimamss@gmail.com>
2022-04-07 15:38:17 +0300

    classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["cloneCar"] }] */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "Currency" }] */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, convertRate) {
+    return amount * convertRate;
+  }
+}
